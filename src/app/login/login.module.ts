a78c7958418a4e5052df1298368cb47fd62a66b5
Andressa <andressa.a.tenorio88@gmail.com>
2021-04-01 23:56:43 -0300

    Configuração do Angular Material

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './components/login/login.component';
-
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatSliderModule
   ]
 })
 export class LoginModule { }
